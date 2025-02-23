8a22b28b28e1cc7633b02f2acb43f24871ec4e45
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-02-23 21:26:34 +0530

    updated files before push

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ async function sendMessage() {
 
 async function getAIResponse(userMessage) {
     const API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill";
-    const API_KEY = "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";
+    const API_KEY = "";
 
     try {
         const response = await fetch(API_URL, {
